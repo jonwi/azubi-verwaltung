8b7f2ded30cefd21ce7d506fb918c818fd1d9be5
Jonas <wiener.jonas1@gmail.com>
2017-09-15 22:17:12 +0200

    Bootstrap local changed layout to divs

--- src/jw/secondproject/client/SecondProject.java
+++ src/jw/secondproject/client/SecondProject.java
@@ -1,15 +1,18 @@
 package jw.secondproject.client;
 
 import java.util.Arrays;
+import java.util.List;
 
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
@@ -31,10 +34,12 @@ public class SecondProject implements EntryPoint {
 
 	private HorizontalPanel bodyWrapper = new HorizontalPanel();
 
-	private VerticalPanel body = new VerticalPanel();
-	
-	private VerticalPanel navigation = new VerticalPanel();
+	private FlowPanel body = new FlowPanel();
 
+	private FlowPanel navigation = new FlowPanel();
+
+	private StudentServiceAsync studentService = GWT.create(StudentService.class);
+	
 	/**
 	 * This is the entry point method.
 	 */
@@ -42,10 +47,14 @@ public class SecondProject implements EntryPoint {
 		RootPanel.get().add(mainPanel);
 		mainPanel.add(headline);
 		headline.add(new Label("Auszubildendensuche"));
+
 		mainPanel.add(bodyWrapper);
+
+		navigation.setStyleName("navbar");
 		bodyWrapper.add(navigation);
-		bodyWrapper.add(body);		
-		
+
+		bodyWrapper.add(body);
+
 		// Navigation
 		navigation.add(new Label("Navigation"));
 		// Suche
@@ -57,38 +66,36 @@ public class SecondProject implements EntryPoint {
 
 	private Panel initializeSuche() {
 		// Suche nach Schuelern
-		VerticalPanel search = new VerticalPanel();
+		FlowPanel search = new FlowPanel();
 		HorizontalPanel searchCrit = new HorizontalPanel();
 		Button suchen = new Button("Suchen");
-		suchen.addStyleName("superSuche");
-		Label labelBetrieb = new Label("Betrieb");
-		TextBox betrieb = new TextBox();
-		Label labelName = new Label("Name");
-		TextBox name = new TextBox();
-		Label labelVorname = new Label("Vorname");
-		TextBox vorname = new TextBox();
-		Label labelKlasse = new Label("Klasse");
-		TextBox klasse = new TextBox();
-		Label labelMatrikelnummer = new Label("Matrikelnummer");
-		TextBox matrikelnummer = new TextBox();
+		suchen.addStyleName("btn btn-primary");
+
 		CheckBox vollzeit = new CheckBox("Vollzeit");
 		vollzeit.setValue(true);
 
-		searchCrit.add(labelBetrieb);
-		searchCrit.add(betrieb);
-		searchCrit.add(labelName);
-		searchCrit.add(name);
-		searchCrit.add(labelVorname);
-		searchCrit.add(vorname);
-		searchCrit.add(labelKlasse);
-		searchCrit.add(klasse);
-		searchCrit.add(labelMatrikelnummer); 
-		searchCrit.add(matrikelnummer);
+		searchCrit.add(createInput("Betrieb"));
+		searchCrit.add(createInput("Name"));
+		searchCrit.add(createInput("Vorname"));
+		searchCrit.add(createInput("Klasse"));
+		searchCrit.add(createInput("Matrikelnummer"));
 		searchCrit.add(vollzeit);
-		
+
 		search.add(searchCrit);
 		search.add(suchen);
 		return search;
+	}
+
+	private FlowPanel createInput(String name) {
+		Label label = new Label(name);
+		label.setStyleName("");
+		TextBox box = new TextBox();
+		box.setStyleName("form-control");
+		FlowPanel pane = new FlowPanel();
+		pane.add(label);
+		pane.add(box);
+		pane.addStyleName("form-group");
+		return pane;
 	}
 
 	private Panel initializeStudentList() {
@@ -101,21 +108,50 @@ public class SecondProject implements EntryPoint {
 		selectionModel.addSelectionChangeHandler(ev -> label.setText(selectionModel.getSelectedObject().toString()));
 
 		studentList.setRowData(0, Arrays.asList(new Student(1), new Student(2), new Student(3)));
-		HorizontalPanel listWrapper = new HorizontalPanel();
+		FlowPanel listWrapper = new FlowPanel();
 		listWrapper.add(label);
 		listWrapper.add(studentList);
 
 		return listWrapper;
 	}
-	
-	 class StudentCell extends AbstractCell<Student> implements Cell<Student>{
+
+	private String wrapDiv(String content, String htmlClass) {
+		return "<div class=\"" + htmlClass + "\">" + content + "</div>";
+	}
+
+	private String wrapDiv(String content) {
+		return "<div>" + content + "</div>";
+	}
+
+	class StudentCell extends AbstractCell<Student> implements Cell<Student> {
 
 		@Override
 		public void render(Context context, Student value, SafeHtmlBuilder sb) {
-			sb.append(context.getColumn());
-			sb.appendEscaped(value.toString());
+			sb.appendHtmlConstant("<div class=\"container row\"><div class=\"col-1\">" + context.getColumn()
+					+ "</div><div class=\"col\">" + wrapDiv(wrapDiv(String.valueOf(value.getNr()), "col") + wrapDiv(value.getBetrieb(), "col") + wrapDiv(value.getName(), "col")
+					+ wrapDiv(value.getVorname(), "col"),"row") + "</div></div>");
 		}
-		 
-	 }
+	}
 	
+	
+	// Service implementation
+	private void getStudents(){
+		if (studentService == null) {
+			studentService = GWT.create(StudentService.class);
+		}
+		
+		AsyncCallback<List<Student>> callback = new AsyncCallback<List<Student>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// assertFailure
+			}
+
+			@Override
+			public void onSuccess(List<Student> result) {
+				
+			}
+		};
+		studentService.getStudents("", "", callback);
+	}
 }
